66fb2dcab320269f2d5cb4c0500fd59e36a0e576
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-01 13:00:46 +0530

    this is the first program of December 2025

--- src/JavaFirstProgram.java
+++ src/JavaFirstProgram.java
@@ -1,8 +1,20 @@
+class Mohan{
+    void fun1(){
+        System.out.println("The quick brown fox jumps over the lazy dog - 6");
+    }
+    void fun(){
+        System.out.println("the quick brown fox jumps over the lazy dog - 5");
+    }
+}
 public class JavaFirstProgram {
     public static void main(String[] args) {
         System.out.println("The quick brown fox jumps over the lazy dog");
         System.out.println("The quick brown fox jumps over the lazy dog");
         System.out.println("The quick brown fox jumps over the lazy dog");
         System.out.println("The quick brown fox jumps over the lazy dog");
+
+        Mohan moh = new Mohan();
+        moh.fun();
+        moh.fun1();
     }
 }
