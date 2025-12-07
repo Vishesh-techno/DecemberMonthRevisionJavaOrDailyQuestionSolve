51d02cf86346b94adfae52dce9c9538533782b51
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-07 20:45:02 +0530

    this is the first program of December 2025

---
+++ src/SevenDecember.java
@@ -0,0 +1,30 @@
+abstract class Animal{
+    abstract void sound();
+    void sleep(){
+        System.out.println("Animal is sleeping...");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void sound() {
+        System.out.println("Dog Barks");
+    }
+}
+class Cat extends Animal{
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class SevenDecember {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        a1.sleep();
+        a1.sound();
+
+        a2.sleep();
+        a2.sound();
+    }
+}
