85df104aebee8c8ff482236853e720fdf1dc138b
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-13 15:49:10 +0530

    Program of today

--- src/ThirteenDecember.java
+++ src/ThirteenDecember.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class ThirteenDecember {
     public static void rotateArray(int[] nums, int k) {
@@ -18,11 +18,47 @@ public class ThirteenDecember {
         }
     }
 
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> duplicate = new HashSet<>();
+
+        for (int num : nums) {
+            if (duplicate.contains(num)) {
+                return true;
+            }
+            duplicate.add(num);
+        }
+
+        return false;
+    }
+
+    public static int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 0);
+            }
+            map.put(num, map.get(num) + 1);
+        }
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] nums = {5, 6, 7, 1, 2, 3, 4};
+        int[] nums1 = {5, 6, 7, 1, 2, 3, 4, 5, 6};
+        int[] nums2 = {1,1,5,5,2,2,6,8,8};
         int k = 4;
-        System.out.println("The Original Array is: " + Arrays.toString(nums));
+        System.out.println("The Original A      rray is: " + Arrays.toString(nums));
         rotateArray(nums, k);
         System.out.println("The Rotated Array is: " + Arrays.toString(nums) + " by " + k + " Steps");
+
+        System.out.println("Is Duplicate Present in Array: " + containsDuplicate(nums));
+
+        System.out.println(singleNumber(nums2));
     }
 }
