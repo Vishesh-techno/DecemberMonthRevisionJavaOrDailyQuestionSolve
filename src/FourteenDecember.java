2ab0372f01c5e3ecf5517c10f2fa562a22cf80c7
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-14 13:19:31 +0530

    Program of today

--- src/FourteenDecember.java
+++ src/FourteenDecember.java
@@ -29,6 +29,26 @@ public class FourteenDecember {
         return res;
     }
 
+    public static int[] intersectionII(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0, k = 0;
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                res[k] = nums1[i];
+                k++;
+                i++;
+                j++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return Arrays.copyOfRange(res, 0, k);
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 2, 1, 7, 9};
         int[] nums2 = {1, 2, 2, 7, 9};
@@ -38,6 +58,8 @@ public class FourteenDecember {
         System.out.println(Arrays.toString(intersection(nums1, nums2)));
 
         System.out.println(Arrays.toString(k));
+
+        System.out.println(Arrays.toString(intersectionII(nums1, nums2)));
 
     }
 }
