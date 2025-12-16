3b1437ec9d5a9975379195cc1362c881968d8937
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-16 20:21:29 +0530

    Program of today

--- src/SixteenDecember.java
+++ src/SixteenDecember.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,12 +16,40 @@ public class SixteenDecember {
     }
 
     public static boolean validMountainArray(int[] arr) {
+        int i = 0, n = arr.length;
 
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+
+    public static int[] replaceElements(int[] nums) {
+        int max = -1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int temp = nums[i];
+            nums[i] = max;
+            max = Math.max(max, temp);
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
         int[] arr = {3, 1, 7, 11};
 
         System.out.println(checkIfExist(arr));
+
+        System.out.println(validMountainArray(arr));
+
+        System.out.println(Arrays.toString(replaceElements(arr)));
     }
 }
