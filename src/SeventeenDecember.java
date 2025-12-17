56306624e2810e941e9f2f34adc5883650ad9e97
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-17 20:14:48 +0530

    Program of today

--- src/SeventeenDecember.java
+++ src/SeventeenDecember.java
@@ -18,8 +18,28 @@ public class SeventeenDecember {
         return -1;
     }
 
+    public static int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int idx = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                idx = i;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != idx && max < 2 * nums[i]) {
+                return -1;
+            }
+        }
+        return idx;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 7, 3, 6, 5, 6};
         System.out.println(findPivotIndex(nums));
+        System.out.println(dominantIndex(nums));
     }
 }
