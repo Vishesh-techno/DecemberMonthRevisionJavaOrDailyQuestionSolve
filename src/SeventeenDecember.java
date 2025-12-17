3ca29d74670e9256fe8adf4b314b83e71176a6e0
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-17 19:58:28 +0530

    Program of today

---
+++ src/SeventeenDecember.java
@@ -0,0 +1,24 @@
+public class SeventeenDecember {
+
+    public static int findPivotIndex(int[] nums){
+        int rightSum = 0, leftSum = 0;
+
+        for(int num: nums){
+            rightSum += num;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(leftSum == rightSum - nums[i]){
+                return i;
+            }
+
+            leftSum += nums[i];
+            rightSum -= nums[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(findPivotIndex(nums));
+    }
+}
