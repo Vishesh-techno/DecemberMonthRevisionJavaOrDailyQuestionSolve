b395fac5341e693b9261b8e2cc94a07fe5067f17
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-19 20:46:18 +0530

    Program of today

--- src/NinteenDecember.java
+++ src/NinteenDecember.java
@@ -30,8 +30,31 @@ public class NinteenDecember {
         return -1;
     }
 
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int track[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            track[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            track[t.charAt(i) - 'a']--;
+        }
+
+        for (int j : track) {
+            if (j != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(reverseNumber(999999999));
         System.out.println(findUniqueCharacter("InfobeansFoundation"));
+        System.out.println(isAnagram("anagram", "naagram"));
     }
 }
