fb082d220f527767f71d452bd94f9cf487f97e05
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-19 20:27:27 +0530

    Program of today

--- src/NinteenDecember.java
+++ src/NinteenDecember.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class NinteenDecember {
     public static int reverseNumber(int num) {
         long rev = 0;
@@ -12,7 +15,23 @@ public class NinteenDecember {
         return (int) rev;
     }
 
+    public static int findUniqueCharacter(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println(reverseNumber(999999999));
+        System.out.println(findUniqueCharacter("InfobeansFoundation"));
     }
 }
