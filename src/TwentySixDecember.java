cfa7854e79e4803edfa30ef1b3bd80493d8d64b7
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-26 13:41:26 +0530

    Program of today

--- src/TwentySixDecember.java
+++ src/TwentySixDecember.java
@@ -150,6 +150,21 @@ public class TwentySixDecember {
         return slow;
     }
 
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+
+        while (temp1 != null) {
+            ListNode temp2 = headB;
+            while (temp2 != null) {
+                if (temp1 == temp2) {
+                    return temp1;
+                }
+                temp2 = temp2.next;
+            }
+            temp1 = temp1.next;
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         TwentySixDecember ll = new TwentySixDecember();
