1720fd9879a58529555d2575bcbe30739134136d
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-29 17:17:47 +0530

    Program of today

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeSort {
     public static void conquer(int[] arr, int start, int mid, int end) {
         int[] merged = new int[end - start + 1];
@@ -45,11 +49,32 @@ public class MergeSort {
         conquer(arr, start, mid, end);
     }
 
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        divide(arr, 0, arr.length - 1);
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 1; i < arr.length; i++) {
+            int diff = arr[i] - arr[i - 1];
+
+            if (diff == minDiff) {
+                res.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = {5, 7, 8, 1, 8, 9, 7};
 
-        divide(arr, 0, arr.length - 1);
+//        divide(arr, 0, arr.length - 1);
 
+        System.out.println(minimumAbsDifference(arr));
         for (int j : arr) {
             System.out.print(j + "==>");
         }
