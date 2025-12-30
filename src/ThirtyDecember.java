d64662405515216b6f494c0ab41401f991b670a9
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-30 15:47:19 +0530

    Program of today

---
+++ src/ThirtyDecember.java
@@ -0,0 +1,24 @@
+public class ThirtyDecember {
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+
+        System.out.println(binarySearch(nums, 9));
+    }
+}
