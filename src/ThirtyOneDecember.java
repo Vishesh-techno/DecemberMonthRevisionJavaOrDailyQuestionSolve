9e10a2ffe1e2e1c9854caab650132181d6102bb0
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-31 13:22:44 +0530

    Program of today

--- src/ThirtyOneDecember.java
+++ src/ThirtyOneDecember.java
@@ -72,6 +72,21 @@ public class ThirtyOneDecember {
         return start;
     }
 
+    public static int findPeak(int[] nums) {
+        int start = 0, end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
     public static void main(String[] args) {
         System.out.println("The picked no. is: " + guessNumber(100));
 
@@ -81,5 +96,6 @@ public class ThirtyOneDecember {
 
         System.out.println("The idx of Rotated Sorted Array is: " + rotatedSortedArray(nums, 2));
         System.out.println("The idx of Minimun in Rotated Sorted Array is: " + findMin(nums1));
+        System.out.println("The idx of Peak Element in Array is: " + findPeak(nums1));
     }
 }
