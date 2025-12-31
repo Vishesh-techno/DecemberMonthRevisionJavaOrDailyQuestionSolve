bf8d757b2698ac29e4d5e00f9c7cfd73fc796da1
Vishesh-techno <sonivishesh099@gmail.com>
2025-12-31 12:55:42 +0530

    Program of today

--- src/ThirtyOneDecember.java
+++ src/ThirtyOneDecember.java
@@ -57,11 +57,29 @@ public class ThirtyOneDecember {
         return -1;
     }
 
+    public static int findMin(int[] nums) {
+        int start = 0, end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
     public static void main(String[] args) {
         System.out.println("The picked no. is: " + guessNumber(100));
 
         int[] nums = {4, 2};
 
+        int[] nums1 = {4, 5, 6, 7, 0, 1, 2};
+
         System.out.println("The idx of Rotated Sorted Array is: " + rotatedSortedArray(nums, 2));
+        System.out.println("The idx of Minimun in Rotated Sorted Array is: " + findMin(nums1));
     }
 }
